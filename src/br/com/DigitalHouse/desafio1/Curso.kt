5879e15261eacd39dcd118def76082bedc54a7c9
TatuSilva <tainamoraes.br@gmail.com>
2020-09-25 19:03:25 -0300

    Parte I - registrarCurso

--- src/br/com/DigitalHouse/desafio1/Curso.kt
+++ src/br/com/DigitalHouse/desafio1/Curso.kt
@@ -1,7 +1,13 @@
 package br.com.DigitalHouse.desafio1
 
-open class Curso (var nome : String = "", var codCurso: Int = 0){
+open class Curso (var nome : String = "", var codCurso: Int = 0, var alunosMax: Int){
   //Um curso será igual a outro se seus códigos de curso forem iguais
+
+//    fun adicionarUmAluno(umAluno: Aluno): Boolean{
+//        cursos = umAluno
+//    }
+
+
   fun print(){
       println("$nome Código: $codCurso")
   }
--- src/br/com/DigitalHouse/desafio1/DigitalHouseManager.kt
+++ src/br/com/DigitalHouse/desafio1/DigitalHouseManager.kt
@@ -1,5 +1,30 @@
 package br.com.DigitalHouse.desafio1
 
-class DigitalHouseManager(var alunos: MutableList<Aluno>, var professores: MutableList<Professor>,
-                          var cursos: MutableList<Curso>, var matriculas: MutableList<Matricula>) {
-}
+class DigitalHouseManager() {
+    var listaAlunos: MutableList<Aluno> = mutableListOf()
+    var listaProfessores: MutableList<Professor> = mutableListOf()
+    var listaCursos: MutableList<Curso> = mutableListOf()
+    var listaMatriculas: MutableList<Matricula> = mutableListOf()
+
+
+    fun registrarCurso(novoCurso: Curso): Collection<Curso> {
+        var codigoCadastrado = false
+        this.listaCursos.forEach{
+            if(it.codCurso == novoCurso.codCurso){
+                codigoCadastrado = true
+            }
+        }
+
+        if(!codigoCadastrado) {
+            listaCursos.add(novoCurso)
+            listaCursos.forEach {
+                println("O curso foi registrado com sucesso!\nCurso: ${it.nome}\nCódigo: ${it.codCurso}\nQuantidade Máxima de Alunos: ${it.alunosMax}")
+                println("-----------------------")
+            }
+        } else {
+                println("Código já cadastrado. Por favor tente novamente!")
+                println("-----------------------")
+        }
+            return listaCursos
+        }
+    }
