9e2d1cdf028d121017ed8588ec6fb7298a6571b9
TatuSilva <tainamoraes.br@gmail.com>
2020-09-25 16:17:27 -0300

    A e B

---
+++ src/br/com/DigitalHouse/desafio1/Aluno.kt
@@ -0,0 +1,6 @@
+package br.com.DigitalHouse.desafio1
+
+class Aluno(var nome: String = "", var sobrenome: String, var codAluno: Int = 0) {
+    //Um aluno será igual a outro se seus códigos de aluno forem iguais
+
+}
---
+++ src/br/com/DigitalHouse/desafio1/Curso.kt
@@ -0,0 +1,5 @@
+package br.com.DigitalHouse.desafio1
+
+open class Curso (var nome : String = "", var codCurso: Int = 0){
+  //Um curso será igual a outro se seus códigos de curso forem iguais
+}
