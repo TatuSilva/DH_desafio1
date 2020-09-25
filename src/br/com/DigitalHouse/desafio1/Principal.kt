20ae2463311b8eccf1d98010eee67f58e6693ffa
TatuSilva <tainamoraes.br@gmail.com>
2020-09-25 17:12:40 -0300

    Parte F

--- src/br/com/DigitalHouse/desafio1/Principal.kt
+++ src/br/com/DigitalHouse/desafio1/Principal.kt
@@ -6,10 +6,15 @@ fun main() {
     val aluno1 = Aluno("Tainá", "Moraes")
     val aluno2 = Aluno("Amanda", "Silva")
 
+    val kotlin = Curso("Kotlin", 1)
+
     val prof1 = ProfessorAdjunto("Bruna", "Lemos", 4, 1,6)
     val prof2 = ProfessorTitular("Paula", "Souza", 2, 2, "Geografia")
+
+    val aluno1Kotlin = Matricula(aluno1, kotlin)
     aluno1.print()
     aluno2.print()
     prof1.print()
     prof2.print()
+    aluno1Kotlin.print()
 }
