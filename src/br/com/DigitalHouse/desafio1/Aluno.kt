e425d6c274c40f8a6218208376691ecaa4928bac
TatuSilva <tainamoraes.br@gmail.com>
2020-09-28 20:12:52 -0300

    Parte I.7

--- src/br/com/DigitalHouse/desafio1/Aluno.kt
+++ src/br/com/DigitalHouse/desafio1/Aluno.kt
@@ -1,14 +1,8 @@
 package br.com.DigitalHouse.desafio1
 
 class Aluno(var nome: String = "", var sobrenome: String, var codAluno: Int = 0) {
-    fun gerarCod(){
-        codAluno ++
-    }
 
     //Um aluno será igual a outro se seus códigos de aluno forem iguais
-      init {
-            gerarCod()
-       }
 
     fun print(){
         println("$nome $sobrenome Código: $codAluno")
