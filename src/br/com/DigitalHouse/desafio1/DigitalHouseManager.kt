035b7cf15d0384c06f56147b420073d7c247c195
TatuSilva <tainamoraes.br@gmail.com>
2020-09-29 18:52:19 -0300

    Parte J e K e Menu até 5

--- src/br/com/DigitalHouse/desafio1/DigitalHouseManager.kt
+++ src/br/com/DigitalHouse/desafio1/DigitalHouseManager.kt
@@ -17,10 +17,11 @@ open class DigitalHouseManager() {
         }
         if(!codigoCadastrado) {
             listaCurso.add(novoCurso)
-                println("O curso foi registrado com sucesso!\nCurso: ${novoCurso.nome}\nCódigo: ${novoCurso.codCurso}\nQuantidade Máxima de Alunos: ${novoCurso.alunosMax}")
+                println("O curso foi registrado com sucesso!\nCurso: ${novoCurso.nome}\nCódigo: ${novoCurso.codCurso}\nQuantidade Máxima de Alunos: ${novoCurso.alunosMax}\n" +
+                        "================================")
             return listaCurso
         } else {
-                println("Código já cadastrado. Por favor tente novamente!")
+                println("Código já cadastrado. Por favor tente novamente!\n================================")
         }
         return listaCurso
         }
@@ -34,10 +35,12 @@ open class DigitalHouseManager() {
                        cursoExcluir = it
                        var index = listaCurso.indexOf(cursoExcluir)
                        listaCurso.removeAt(index)
-                       println("Curso ${cursoExcluir.nome} excluído com sucesso!")
+                       println("Curso ${cursoExcluir.nome} excluído com sucesso!\n" +
+                               "================================")
                    }
                }
-               if(cursoExcluir.nome == "Erro")  println("Curso não encontrado. Tente novamente.")
+               if(cursoExcluir.nome == "Erro")  println("Curso não encontrado. Tente novamente.\n" +
+                       "================================")
            }catch (ex: Exception){
               // ENTENDER PQ ESTÁ CAINDO EM CATCH NO PRIMEIRO E ÚLTIMO INDEX
            }
@@ -46,8 +49,9 @@ open class DigitalHouseManager() {
 
 //IMPRIME LISTA CURSOS
         open fun listaCursosPrint(){
-            listaCurso.forEach{
-                println("${it.codCurso} - ${it.nome}")
+           return listaCurso.forEach{
+                println("${it.codCurso} - ${it.nome}\n" +
+                        "======================")
             }
         }
 
@@ -62,10 +66,12 @@ open class DigitalHouseManager() {
         }
         if(!codigoCadastrado) {
             listaProfessores.add(novoProf)
-            println("O Professor(a) Adjunto/a foi registrado com sucesso!\nNome Completo: ${novoProf.nome} ${novoProf.sobrenome}\nCódigo: ${novoProf.codProfessor}\nHoras de monitoria: ${novoProf.horasMonitoria}")
+            println("O Professor(a) Adjunto/a foi registrado com sucesso!\nNome Completo: ${novoProf.nome} ${novoProf.sobrenome}\nCódigo: ${novoProf.codProfessor}\nHoras de monitoria: ${novoProf.horasMonitoria}\n" +
+                    "================================")
             return listaProfessores
         } else {
-            println("Professor já cadastrado. Por favor tente novamente!")
+            println("Professor já cadastrado. Por favor tente novamente!\n" +
+                    "================================")
         }
         return listaProfessores
     }
@@ -79,10 +85,12 @@ open class DigitalHouseManager() {
             }
             if(!codigoCadastrado) {
                 listaProfessores.add(novoProf)
-                println("O Professor(a) Titular foi registrado com sucesso!\nNome Completo: ${novoProf.nome} ${novoProf.sobrenome}\nCódigo: ${novoProf.codProfessor}\nEspecialidade: ${novoProf.especialidade}")
+                println("O Professor(a) Titular foi registrado com sucesso!\nNome Completo: ${novoProf.nome} ${novoProf.sobrenome}\nCódigo: ${novoProf.codProfessor}\nEspecialidade: ${novoProf.especialidade}\n" +
+                        "================================")
                 return listaProfessores
             } else {
-                println("Código de professor já cadastrado. Por favor tente novamente!")
+                println("Código de professor já cadastrado. Por favor tente novamente!\n" +
+                        "================================")
             }
             return listaProfessores
         }
@@ -97,13 +105,15 @@ open class DigitalHouseManager() {
                     profExcluir = it
                     var index = listaProfessores.indexOf(profExcluir)
                     listaProfessores.removeAt(index)
-                    println("Professor(a) ${profExcluir.nome} ${profExcluir.sobrenome} excluído com sucesso!")
+                    println("Professor(a) ${profExcluir.nome} ${profExcluir.sobrenome} excluído com sucesso!\n" +
+                            "================================")
                     naoEncontrado = 1
                 }else{
                     naoEncontrado = 0
                 }
             }
-            if(naoEncontrado == 0)  println("Professor não encontrado. Tente novamente.")
+            if(naoEncontrado == 0)  println("Professor não encontrado. Tente novamente.\n" +
+                    "================================")
         }catch (ex: Exception){
             // ENTENDER PQ ESTÁ CAINDO EM CATCH NO PRIMEIRO E ÚLTIMO INDEX
         }
@@ -112,9 +122,11 @@ open class DigitalHouseManager() {
 //IMPRIME LISTA PROFESSOR
     open fun listaProfPrint(){
         listaProfessores.forEach{
-            println("${it.codProfessor} - ${it.nome} ${it.sobrenome}")
+            println("${it.codProfessor} - ${it.nome} ${it.sobrenome}\n" +
+                    "======================")
         }
     }
+
 //MATRICULAR ALUNO
     fun matricularAluno (novoAluno: Aluno): Collection<Aluno>{
     var codigoCadastrado = false
@@ -125,14 +137,50 @@ open class DigitalHouseManager() {
     }
     if(!codigoCadastrado) {
         listaAlunos.add(novoAluno)
-        println("Aluno(a) foi registrado com sucesso!\nNome Completo: ${novoAluno.nome} ${novoAluno.sobrenome}\nCódigo: ${novoAluno.codAluno}")
+        println("Aluno(a) foi registrado com sucesso!\nNome Completo: ${novoAluno.nome} ${novoAluno.sobrenome}\nCódigo: ${novoAluno.codAluno}\n" +
+                "================================")
         return listaAlunos
     } else {
-        println("Código de aluno já cadastrado. Por favor tente novamente!")
+        println("Código de aluno já cadastrado. Por favor tente novamente!\n" +
+                "================================")
     }
     return listaAlunos
     }
-//MATRICULAR ALUNO NO CURSO
+
+    //EXCLUIR ALUNO
+
+    open fun excluirAluno(codExcluir: Int): MutableList<Aluno> {
+        var alunoExcluir: Aluno
+        var naoEncontrado: Int = 0
+        try {
+            this.listaAlunos.forEach {
+                if (it.codAluno == codExcluir.toInt()) {
+                    alunoExcluir = it
+                    var index = listaAlunos.indexOf(alunoExcluir)
+                    listaAlunos.removeAt(index)
+                    println("Aluno(a) ${alunoExcluir.nome} ${alunoExcluir.sobrenome} excluído com sucesso!\n" +
+                            "================================")
+                    naoEncontrado = 1
+                }else{
+                    naoEncontrado = 0
+                }
+            }
+            if(naoEncontrado == 0)  println("Aluno(a) não encontrado. Tente novamente.\n" +
+                    "================================")
+        }catch (ex: Exception){
+            // ENTENDER PQ ESTÁ CAINDO EM CATCH NO PRIMEIRO E ÚLTIMO INDEX
+        }
+        return listaAlunos
+    }
+    //IMPRIME LISTA ALUNO
+    open fun listaAlunoPrint(){
+        listaAlunos.forEach{
+            println("${it.codAluno} - ${it.nome} ${it.sobrenome}\n" +
+                    "======================")
+        }
+    }
+
+    //MATRICULAR ALUNO NO CURSO
     fun matricularAlunoCurso(codigoAluno: Int, codigoCurso: Int): MutableList<Matricula> {
 
         var procuraCurso: Curso = Curso("", 0, 0)
@@ -150,19 +198,23 @@ open class DigitalHouseManager() {
 
         if(procuraAluno.codAluno == 0)
         {
-            println("Aluno não encontrado")
+            println("Aluno não encontrado\n" +
+                    "================================")
             return listaMatriculas
         }
         if(procuraCurso.codCurso == 0) {
-            println("Curso não encontrado")
+            println("Curso não encontrado\n" +
+                    "================================")
             return listaMatriculas
         }
         if(procuraCurso.alunosMax > procuraCurso.listaAlunosNoCurso.size) {
             procuraCurso.adicionarUmAluno(procuraAluno)
             listaMatriculas.add(Matricula(procuraAluno,procuraCurso))
-            println("Matrícula realizada com sucesso!\n${procuraAluno.nome} foi matriculado(a) no curso ${procuraCurso.nome}")
+            println("Matrícula realizada com sucesso!\n${procuraAluno.nome} foi matriculado(a) no curso ${procuraCurso.nome}\n" +
+                    "================================")
         }else {
-            println("Não foi possível realizar a matrícula.\nNão há mais vagas disponíveis para este curso.")
+            println("Não foi possível realizar a matrícula.\nNão há mais vagas disponíveis para este curso.\n" +
+                    "================================")
         }
         return listaMatriculas
         }
@@ -193,31 +245,47 @@ open class DigitalHouseManager() {
 
                 if(procuraProfTitular.codProfessor != 0 && procuraProfAdjunto.codProfessor != 0 && procuraCurso.codCurso != 0){
                     procuraCurso.adicionarProfessores(procuraProfTitular, procuraProfAdjunto)
-                    println("Professor(a) alocado com sucesso!" +
+                    println("Professores alocados com sucesso!" +
                             "\nCurso: ${procuraCurso.nome}" +
                             "\nProfessor(a) Titular: ${procuraProfTitular.nome} ${procuraProfTitular.sobrenome}" +
-                            "\nProfessor(a) Adjunto(a): ${procuraProfAdjunto.nome} ${procuraProfAdjunto.sobrenome}")
+                            "\nProfessor(a) Adjunto(a): ${procuraProfAdjunto.nome} ${procuraProfAdjunto.sobrenome}\n" +
+                            "================================")
                     return listaProfessores
                 }
 
             if(procuraCurso.codCurso == 0) {
-                println("Curso não encontrado")
+                println("Curso não encontrado\n" +
+                        "================================")
                 return listaProfessores
             }
             if(procuraProfTitular.codProfessor == 0 )
             {
-                println("Professor(a) titular não encontrado(a)")
+                println("Professor(a) titular não encontrado(a)\n" +
+                        "================================")
                 return listaProfessores
             }
             if(procuraProfAdjunto.codProfessor == 0)
             {
-                println("Professor(a) adjunto não encontrado(a)")
+                println("Professor(a) adjunto não encontrado(a)\n" +
+                        "================================")
                 return listaProfessores
             }
             }catch (ex: Exception){
-                println("Algo deu errado. Verifique os códigos digitados.")
+                println("Algo deu errado. Verifique os códigos digitados.\n" +
+                        "================================")
             }
             return listaProfessores
         }
+        fun consultarCursoAluno(codigoAluno: Int){
+            var procuraAlunoCurso = Aluno("","", 0)
+            listaMatriculas.forEach {
+                if(codigoAluno.toInt() == it.aluno.codAluno){
+                    procuraAlunoCurso = it.aluno
+                    println("Aluno(a) ${it.aluno.nome} está matriculado no curso ${it.curso.nome}")
+                }
+            }
+            if(procuraAlunoCurso.codAluno == 0) println("Aluno não encontrado ou aluno não está matriculado em nenhum curso")
         }
+        }
+
 
--- src/br/com/DigitalHouse/desafio1/Principal.kt
+++ src/br/com/DigitalHouse/desafio1/Principal.kt
@@ -1,47 +1,248 @@
 package br.com.DigitalHouse.desafio1
 
+import java.time.Year
+import java.util.*
+
 class Principal {
 }
 fun main() {
+
     val manager = DigitalHouseManager()
-    val aluno1 = Aluno("Tainá", "Moraes",1)
-    val aluno2 = Aluno("Amanda", "Silva",2)
-    val aluno3 = Aluno("Pamela", "Carvalho", 3)
 
-    val android = Curso("Android", 1, 5)
-    val kotlin = Curso("Kotlin", 2, 3)
-    val java = Curso("Java", 3, 2)
+    val titular1 = ProfessorTitular("Amanda", "Silva", 2, 10001,"Hardware")
+    val titular2 = ProfessorTitular("Paula","Carvalho", 4, 10002, "Kotlin")
+    val adjunto1 = ProfessorAdjunto("Alfredo", "Souza", 1, 10003, 3)
+    val adjunto2 = ProfessorAdjunto("Cristina", "Alvarez", 3, 10004, 5)
 
-    val adjunto1 = ProfessorAdjunto("Bruna", "Lemos", 4, 1,6)
-    val titular1 = ProfessorTitular("Paula", "Souza", 2, 2, "Geografia")
+    val fullStack = Curso("Full Stack", 20001, 3)
+    val android = Curso("Android", 20002, 2)
 
-    manager.registrarCurso(android)
-    println("========================")
-    manager.registrarCurso(kotlin)
-    println("========================")
-    manager.registrarCurso(java)
-    println("========================")
+    val aluno1 = Aluno("José", "Ribeiro", 30001)
+    val aluno2 = Aluno("Maria", "Ferreira", 30002)
+    val aluno3 = Aluno("Alvaro", "Costa", 30003)
+    val aluno4 = Aluno("Evelyn", "Souza", 30004)
+    val aluno5 = Aluno("Fernanda", "Almeida", 30005)
 
-    //kotlin.adicionarUmAluno(aluno1)
-
-    manager.matricularAluno(aluno1)
-    println("========================")
-    manager.matricularAluno(aluno2)
-    println("========================")
-    manager.matricularAluno(aluno3)
-    println("========================")
-
-    manager.matricularAlunoCurso(1,3)
-    println("========================")
-    manager.matricularAlunoCurso(2,3)
-    println("========================")
-    manager.matricularAlunoCurso(3,3)
-    println("========================")
-
-    manager.registrarProfessorTitular(titular1)
-    println("========================")
     manager.registrarProfessorAdjunto(adjunto1)
-    println("========================")
+    manager.registrarProfessorAdjunto(adjunto2)
+    manager.registrarProfessorTitular(titular1)
+    manager.registrarProfessorTitular(titular2)
+    manager.registrarCurso(fullStack)
+    manager.registrarCurso(android)
+    manager.alocarProfessores(20001, 10001, 10003)
+    manager.alocarProfessores(20002, 10002, 10004)
+    manager.matricularAluno(aluno1)
+    manager.matricularAluno(aluno2)
+    manager.matricularAluno(aluno3)
+    manager.matricularAluno(aluno4)
+    manager.matricularAluno(aluno5)
+    manager.matricularAlunoCurso(30001,20001)
+    manager.matricularAlunoCurso(30002,20001)
+    manager.matricularAlunoCurso(30003,20002)
+    manager.matricularAlunoCurso(30004,20002)
+    manager.matricularAlunoCurso(30005,20002)
 
-    manager.alocarProfessores(1,2,1)
+    //MENU
+    var menu = ""
+    while (menu != "sair") {
+        println("BEM VINDO(A) AO SISTEMA DIGITAL HOUSE\n" +
+                "O QUE VOCÊ GOSTARIA DE FAZER HOJE?\n"+
+                "====================================\n" +
+                " 1 -> ADICIONAR UM NOVO CURSO\n" +
+                " 2 -> EXCLUIR UM CURSO\n" +
+                " 3 -> LISTAR CURSOS CADASTRADOS\n" +
+                " 4 -> ADICIONAR UM NOVO PROFESSOR\n" +
+                " 5 -> ALOCAR PROFESSOR EM UM CURSO\n" +
+                " 6 -> EXCLUIR UM PROFESSOR\n" +
+                " 7 -> LISTAR PROFESSORES CADASTRADOS\n" +
+                " 8 -> ADICIONAR UM NOVO ALUNO\n" +
+                " 9 -> MATRICULAR UM ALUNO EM UM CURSO\n" +
+                "10 -> EXCLUIR UM ALUNO\n" +
+                "11 -> LISTAR ALUNOS CADASTRADOS\n" +
+                "12 -> CONSULTAR CURSO EM QUE ALUNO ESTÁ MATRICULADO\n"+
+                "sair -> SAIR\n" +
+                "====================================\n" +
+                "DIGITE UMA OPÇÃO E APERTE ENTER:\n")
+
+        menu = readLine()!!.toString()
+
+        when (menu) {
+            "1" -> {
+                try{
+                    println("NOME DO CURSO:")
+                    var inputNomeCurso = readLine()!!.toString()
+                    println("CÓDIGO DO CURSO:")
+                    var inputCodigoCurso = readLine()!!.toInt()
+                    println("QUANTIDADE MÁXIMA DE ALUNOS:")
+                    var inputQntAlunos = readLine()!!.toInt()
+                    var novoCurso = Curso(inputNomeCurso, inputCodigoCurso, inputQntAlunos)
+                    manager.registrarCurso(novoCurso)
+                }catch (ex: Exception){
+                    println("Algo deu errado :(\n Tente novamente.")
+            }
+            }
+            "2" -> {
+                try{
+                    println("CÓDIGO DO CURSO  A SER EXCLUÍDO:")
+                    var inputExcluirCurso = readLine()!!.toInt()
+                    manager.excluirCurso(inputExcluirCurso)
+                }catch (ex: Exception){
+                    println("Algo deu errado :(\n Tente novamente.")
+            }
+            }
+            "3" -> {
+                try{
+                   manager.listaCursosPrint()
+                }catch (ex: Exception){
+                    println("Algo deu errado :(\n Tente novamente.")
+            }
+            }
+            "4" -> {
+                try {
+                    println("PARA PROFESSOR ADJUNTO DIGITE 1 E PARA PROFESSOR TITULAR DIGITE 2")
+                    var tipoProf = readLine()!!.toInt()
+                    when (tipoProf) {
+                    1 -> //ADJUNTO
+                    {println("NOME DO PROFESSOR:")
+                    var inputNomeProf = readLine()!!.toString()
+                    println("SOBRENOME DO PROFESSOR:")
+                    var inputSobrenomeProf = readLine()!!.toString()
+                    println("ANO DE INGRESSO NA DIGITAL HOUSE (YYYY)")
+                        if(readLine()!!.toInt() > Year.now().getValue() || readLine()!!.toInt() <= 1900) println("O ano foi digitado errado. Tente novamente")
+                    var tempoCasa= Year.now().getValue()-readLine()!!.toInt()
+                    println("CÓDIGO DO PROFESSOR:")
+                    var inputCodigoProf = readLine()!!.toInt()
+                    println("HORAS DE MONITORIA:")
+                    var inputHoras = readLine()!!.toInt()
+                    var novoProfessorAdjunto = ProfessorAdjunto(inputNomeProf, inputSobrenomeProf, tempoCasa, inputCodigoProf, inputHoras)
+                    manager.registrarProfessorAdjunto(novoProfessorAdjunto)}
+                    2 ->       //TITULAR
+                    {println("NOME DO PROFESSOR:")
+                    var inputNomeProf = readLine()!!.toString()
+                    println("SOBRENOME DO PROFESSOR:")
+                    var inputSobrenomeProf = readLine()!!.toString()
+                    println("ANO DE INGRESSO NA DIGITAL HOUSE (YYYY)")
+                        if(readLine()!!.toInt() > Year.now().getValue() || readLine()!!.toInt() <= 1900) println("O ano foi digitado errado. Tente novamente")
+                        var tempoCasa= Year.now().getValue()-readLine()!!.toInt()
+                    println("CÓDIGO DO PROFESSOR:")
+                    var inputCodigoProf = readLine()!!.toInt()
+                    println("ESPECIALIDADE:")
+                    var inputEspecialidade = readLine()!!.toString()
+                    var novoProfessorTitular = ProfessorTitular(inputNomeProf, inputSobrenomeProf, tempoCasa, inputCodigoProf, inputEspecialidade)
+                    manager.registrarProfessorTitular(novoProfessorTitular)}
+                }
+                    } catch (ex: Exception){
+                        println("Algo deu errado :(\n Tente novamente.")
+                    }
+                }
+            "5" -> {
+                try{
+                    println("CÓDIGO DO CURSO:")
+                    var inputCodCurso = readLine()!!.toInt()
+                    println("CÓDIGO DO PROFESSOR ADJUNTO:")
+                    var inputCodAdjunto = readLine()!!.toInt()
+                    println("CÓDIGO DO PROFESSOR TITULAR:")
+                    var inputCodTitular = readLine()!!.toInt()
+                    manager.alocarProfessores(inputCodCurso, inputCodTitular, inputCodAdjunto)
+                }catch (ex: Exception){
+                    println("Algo deu errado :(\n Tente novamente.")
+            }
+            }
+            "6" -> {
+                try{
+                    println("NOME DO CURSO:")
+                    var inputNomeCurso = readLine()!!.toString()
+                    println("CÓDIGO DO CURSO:")
+                    var inputCodigoCurso = readLine()!!.toInt()
+                    println("QUANTIDADE MÁXIMA DE ALUNOS:")
+                    var inputQntAlunos = readLine()!!.toInt()
+                    var novoCurso = Curso(inputNomeCurso, inputCodigoCurso, inputQntAlunos)
+                    manager.registrarCurso(novoCurso)
+                }catch (ex: Exception){
+                    println("Algo deu errado :(\n Tente novamente.")
+            }
+            }
+            "7" -> {
+                try{
+                    println("NOME DO CURSO:")
+                    var inputNomeCurso = readLine()!!.toString()
+                    println("CÓDIGO DO CURSO:")
+                    var inputCodigoCurso = readLine()!!.toInt()
+                    println("QUANTIDADE MÁXIMA DE ALUNOS:")
+                    var inputQntAlunos = readLine()!!.toInt()
+                    var novoCurso = Curso(inputNomeCurso, inputCodigoCurso, inputQntAlunos)
+                    manager.registrarCurso(novoCurso)
+                }catch (ex: Exception){
+                    println("Algo deu errado :(\n Tente novamente.")
+            }
+            }
+            "8" -> {
+                try{
+                    println("NOME DO CURSO:")
+                    var inputNomeCurso = readLine()!!.toString()
+                    println("CÓDIGO DO CURSO:")
+                    var inputCodigoCurso = readLine()!!.toInt()
+                    println("QUANTIDADE MÁXIMA DE ALUNOS:")
+                    var inputQntAlunos = readLine()!!.toInt()
+                    var novoCurso = Curso(inputNomeCurso, inputCodigoCurso, inputQntAlunos)
+                    manager.registrarCurso(novoCurso)
+                }catch (ex: Exception){
+                    println("Algo deu errado :(\n Tente novamente.")
+            }
+            }
+            "9" -> {
+                try{
+                    println("NOME DO CURSO:")
+                    var inputNomeCurso = readLine()!!.toString()
+                    println("CÓDIGO DO CURSO:")
+                    var inputCodigoCurso = readLine()!!.toInt()
+                    println("QUANTIDADE MÁXIMA DE ALUNOS:")
+                    var inputQntAlunos = readLine()!!.toInt()
+                    var novoCurso = Curso(inputNomeCurso, inputCodigoCurso, inputQntAlunos)
+                    manager.registrarCurso(novoCurso)
+                }catch (ex: Exception){
+                    println("Algo deu errado :(\n Tente novamente.")
+            }
+            }
+            "10" -> {
+                try{
+                    println("NOME DO CURSO:")
+                    var inputNomeCurso = readLine()!!.toString()
+                    println("CÓDIGO DO CURSO:")
+                    var inputCodigoCurso = readLine()!!.toInt()
+                    println("QUANTIDADE MÁXIMA DE ALUNOS:")
+                    var inputQntAlunos = readLine()!!.toInt()
+                    var novoCurso = Curso(inputNomeCurso, inputCodigoCurso, inputQntAlunos)
+                    manager.registrarCurso(novoCurso)
+                }catch (ex: Exception){
+                    println("Algo deu errado :(\n Tente novamente.")
+            }
+            }
+            "11" -> {
+                try{
+                    println("NOME DO CURSO:")
+                    var inputNomeCurso = readLine()!!.toString()
+                    println("CÓDIGO DO CURSO:")
+                    var inputCodigoCurso = readLine()!!.toInt()
+                    println("QUANTIDADE MÁXIMA DE ALUNOS:")
+                    var inputQntAlunos = readLine()!!.toInt()
+                    var novoCurso = Curso(inputNomeCurso, inputCodigoCurso, inputQntAlunos)
+                    manager.registrarCurso(novoCurso)
+                }catch (ex: Exception){
+                    println("Algo deu errado :(\n Tente novamente.")
+            }
+            }
+            "12" -> {
+                try{
+                    println("CÓDIGO ALUNO:")
+                    var codAlunoConsulta = readLine()!!.toInt()
+                    manager.consultarCursoAluno(codAlunoConsulta)
+                }catch (ex: Exception){
+                    println("Algo deu errado :(\n Tente novamente.")
+            }
+            }
+        }
+
+    }
 }
+
